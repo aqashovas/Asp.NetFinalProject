beeaead8c79a2a7001ef3aa7583b24f2fee758dd
Sabina Aqashova <aqashova@inbox.ru>
2019-03-30 00:31:42 +0400

    SomeAddedAdminPanelAndLayout

---
+++ AspNetFinalProject/Areas/Manage/Controllers/SettingsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AspNetFinalProject.Models;
+
+namespace AspNetFinalProject.Areas.Manage.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private PromediHospitalContext db = new PromediHospitalContext();
+
+        // GET: Manage/Settings
+        public ActionResult Index()
+        {
+            return View(db.Settings.ToList());
+        }
+
+        // GET: Manage/Settings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Setting setting = db.Settings.Find(id);
+            if (setting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(setting);
+        }
+
+        // GET: Manage/Settings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manage/Settings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Logo,Linkedin,Facebook,Instagram,Twitter,GooglePlus,Mail,Phone,Lat,Lng,Location,ContactMail1,ContactPhone1,ContactMail2,ContactPhone2")] Setting setting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Settings.Add(setting);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(setting);
+        }
+
+        // GET: Manage/Settings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Setting setting = db.Settings.Find(id);
+            if (setting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(setting);
+        }
+
+        // POST: Manage/Settings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Logo,Linkedin,Facebook,Instagram,Twitter,GooglePlus,Mail,Phone,Lat,Lng,Location,ContactMail1,ContactPhone1,ContactMail2,ContactPhone2")] Setting setting)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(setting).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(setting);
+        }
+
+        // GET: Manage/Settings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Setting setting = db.Settings.Find(id);
+            if (setting == null)
+            {
+                return HttpNotFound();
+            }
+            return View(setting);
+        }
+
+        // POST: Manage/Settings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Setting setting = db.Settings.Find(id);
+            db.Settings.Remove(setting);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AspNetFinalProject/Areas/Manage/Controllers/SpecialitiesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AspNetFinalProject.Models;
+
+namespace AspNetFinalProject.Areas.Manage.Controllers
+{
+    public class SpecialitiesController : Controller
+    {
+        private PromediHospitalContext db = new PromediHospitalContext();
+
+        // GET: Manage/Specialities
+        public ActionResult Index()
+        {
+            var specialities = db.Specialities.Include(s => s.DepartmentCategory);
+            return View(specialities.ToList());
+        }
+
+        // GET: Manage/Specialities/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Speciality speciality = db.Specialities.Find(id);
+            if (speciality == null)
+            {
+                return HttpNotFound();
+            }
+            return View(speciality);
+        }
+
+        // GET: Manage/Specialities/Create
+        public ActionResult Create()
+        {
+            ViewBag.DepartmentCategoryId = new SelectList(db.DepartmentCategories, "Id", "Name");
+            return View();
+        }
+
+        // POST: Manage/Specialities/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,DepartmentCategoryId")] Speciality speciality)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Specialities.Add(speciality);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DepartmentCategoryId = new SelectList(db.DepartmentCategories, "Id", "Name", speciality.DepartmentCategoryId);
+            return View(speciality);
+        }
+
+        // GET: Manage/Specialities/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Speciality speciality = db.Specialities.Find(id);
+            if (speciality == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DepartmentCategoryId = new SelectList(db.DepartmentCategories, "Id", "Name", speciality.DepartmentCategoryId);
+            return View(speciality);
+        }
+
+        // POST: Manage/Specialities/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,DepartmentCategoryId")] Speciality speciality)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(speciality).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DepartmentCategoryId = new SelectList(db.DepartmentCategories, "Id", "Name", speciality.DepartmentCategoryId);
+            return View(speciality);
+        }
+
+        // GET: Manage/Specialities/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Speciality speciality = db.Specialities.Find(id);
+            if (speciality == null)
+            {
+                return HttpNotFound();
+            }
+            return View(speciality);
+        }
+
+        // POST: Manage/Specialities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Speciality speciality = db.Specialities.Find(id);
+            db.Specialities.Remove(speciality);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
